86676ad34b9900defc4d79cf216e05503e163ed4
Jared Kong <jlkong001@yahoo.com>
2021-02-18 13:07:58 -0800

    changed present

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
 presentImg = document.getElementById("present");
+presentImgTwo = document.getElementById("presentTwo");
 belowWords = document.getElementById("belowWords");
 belowWordsTwo = document.getElementById("belowWordsTwo");
 
 presentImg.onclick = function(){
     presentImg.className += " disappear"
+    presentImgTwo.className ="img-fluid"
     belowWords.className += " disappear"
     belowWordsTwo.className -="disappear"
 }
